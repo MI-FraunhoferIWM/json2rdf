b968b9998257433d9f9fd15d4c33b55f7ce47c34
deepukr007 <deepukreddy123@gmail.com>
2024-01-17 10:25:05 +0000

    docker changes

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -1,17 +1,15 @@
 
 import os , sys
 from fastapi.testclient import TestClient
+#import requests
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__) ))
 sys.path.append(parent_dir)
-
 from main import app
-
-
 client = TestClient(app)
 
 
-def test_yarrrml_parser():
+def test_yarrrml_parser_url():
     json_data = {
         'mapping_url': 'https://raw.githubusercontent.com/MI-FraunhoferIWM/json2rdf_data/main/1_mapping.yaml',
     }
@@ -19,7 +17,7 @@ def test_yarrrml_parser():
     response = client.post('http://localhost:5004/api/url/yarrrmltorml', json=json_data)
     assert response.status_code == 200
 
-def test_mapper():
+def test_mapper_url():
     json_data = {
         'data_url': 'https://raw.githubusercontent.com/MI-FraunhoferIWM/json2rdf_data/main/1_data.json',
         'mapping_url': 'https://raw.githubusercontent.com/MI-FraunhoferIWM/json2rdf_data/main/1_mapping.yaml',
